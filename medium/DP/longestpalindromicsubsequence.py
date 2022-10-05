56b4deda446eb3f4b44292a67a3662c09afab176
Kelvin <chuakid@gmail.com>
2022-10-05 14:19:04 +0800

    explanation improvement

--- medium/DP/longestpalindromicsubsequence.py
+++ medium/DP/longestpalindromicsubsequence.py
@@ -9,10 +9,10 @@ class Solution:
             dp[i][i] = 1
             for j in range(i-1, -1, -1):
                 if s[i] == s[j]:
-                    # if right == left, then longest from left to right == left+1 to right-1 + 2
+                    # if right == left, then longest from left to right == left+1 to right-1 + 2 (include both left and right)
                     dp[j][i] = dp[j+1][i-1] + 2
                 else:
-                    # else, longest from left to right == max(left+1 to right, left to right - 1)
+                    # else, longest from left to right == max(left+1 to right (include right), left to right - 1(include left))
                     dp[j][i] = max(dp[j+1][i], dp[j][i-1])
         return dp[0][strlen-1]
 
